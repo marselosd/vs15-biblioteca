81e412205973e2e0c0078b5a7277949e33ae719e
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2024-11-27 15:56:26 -0300

    add

--- Biblioteca/src/Main.java
+++ Biblioteca/src/Main.java
@@ -1,5 +1,18 @@
 public class Main
 {
+    public void buscarLivro(String titulo, Livro[] livros)
+    {
+        for(Livro livro : livros)
+        {
+            if(livro.getTitulo().equalsIgnoreCase(titulo))
+            {
+                System.out.println("Titulo: " + livro.getTitulo() + '\n' +
+                                    "Autor: " + livro.getAutor() + '\n' +
+                                    "Ano de Publicação: " + livro.getAnoPublicacao() + '\n' +
+                                    "Disponibilidade: " + livro.isDisponibilidade() + '\n');
+            }
+        }
+    }
     public static void main(String[] args)
     {
 
